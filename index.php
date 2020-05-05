22085c5a04c1c937b0c298ba4bd25adecdf403f1
Zakariya TRIFISS <Zedaka78@gmail.com>
2020-05-05 14:26:40 +0200

    modif index.php ajout carousel

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,36 +8,64 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
-    
+
 
         <h1 class="alert alert-secondary text-center mt-3">Mon Site GitHub</h1>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis 
-        maxime ea alias numquam accusantium praesentium consectetur.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur, 
-        reiciendis amet aperiam iste laboriosam laudantium. 
-        Dolores dolorum iste nisi explicabo alias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis
+            maxime ea alias numquam accusantium praesentium consectetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur,
+            reiciendis amet aperiam iste laboriosam laudantium.
+            Dolores dolorum iste nisi explicabo alias.</p>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis 
-        maxime ea alias numquam accusantium praesentium consectetur.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur, 
-        reiciendis amet aperiam iste laboriosam laudantium. 
-        Dolores dolorum iste nisi explicabo alias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis
+            maxime ea alias numquam accusantium praesentium consectetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur,
+            reiciendis amet aperiam iste laboriosam laudantium.
+            Dolores dolorum iste nisi explicabo alias.</p>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis 
-        maxime ea alias numquam accusantium praesentium consectetur.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur, 
-        reiciendis amet aperiam iste laboriosam laudantium. 
-        Dolores dolorum iste nisi explicabo alias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Adipisci perferendis optio exercitationem in unde nostrum voluptatibus magnam deleniti iusto, voluptatem, expedita debitis perspiciatis
+            maxime ea alias numquam accusantium praesentium consectetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus consequatur minima facere explicabo odio assumenda quia ad consequuntur,
+            reiciendis amet aperiam iste laboriosam laudantium.
+            Dolores dolorum iste nisi explicabo alias.</p>
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://picsum.photos/id/237/1200/900" class="d-block w-100" style="height: 50vh; !important" alt="https://picsum.photos/id/237/1200/900">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://picsum.photos/id/238/1200/900" class="d-block w-100" style="height: 50vh; !important" alt="https://picsum.photos/id/237/1200/900">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://picsum.photos/id/235/1200/900" class="d-block w-100" style="height: 50vh; !important" alt="https://picsum.photos/id/237/200/300">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
 
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
+
 </html>
